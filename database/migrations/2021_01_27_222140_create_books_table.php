c853efd71ba7b6f7a6e1993f3666b2fdc5935fa5
MarkAwad93 <78170489+MarkAwad93@users.noreply.github.com>
2021-01-28 13:23:27 -0500

    Initial commit

---
+++ database/migrations/2021_01_27_222140_create_books_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('google_id');
+            $table->text('description');
+            $table->string('title');
+            $table->string('author');
+            $table->string('isbn');
+            $table->string('publisher');
+            $table->string('image');
+            $table->unsignedBigInteger('userOrder');
+            $table->timestamps();
+
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
