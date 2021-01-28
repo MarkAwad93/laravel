33d2bc2f0a75ab5b17385c942cd6d4bd238c81cd
MarkAwad93 <78170489+MarkAwad93@users.noreply.github.com>
2021-01-28 14:01:27 -0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://vast-gorge-54359.herokuapp.com/public/'),
 
     'asset_url' => env('ASSET_URL', null),
 
